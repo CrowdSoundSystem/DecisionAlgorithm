c8424fd20e4b08f5502ad968a8b8f584d95fc725
farova <michaela.farova@gmail.com>
2015-07-21 14:02:05 -0400

    Finishing up Tier Multiplier logic

--- DecisionAlgorithm/src/Modules/TierModule.cpp
+++ DecisionAlgorithm/src/Modules/TierModule.cpp
@@ -5,9 +5,14 @@ void TierModule::run(DecisionSettings& settings, MusicDataList& musicDataSet) {
 	MusicDataList::iterator setItr;
 	for (setItr = musicDataSet.begin(); setItr != musicDataSet.end(); ++setItr) {
 
+		Tier tier = (*setItr)->getTier();
+		float multiplier = 1.0;
 
+		if (settings.m_tierMultipliers.count(tier) == 1) {
+			multiplier = settings.m_tierMultipliers[tier];
+		}
 
-		(*setItr)->mulWeight();
+		(*setItr)->mulWeight(multiplier);
 	}
 
 }
