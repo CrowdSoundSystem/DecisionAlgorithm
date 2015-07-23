4768e11e37b5a84c1e57440a491490ed37b9460d
farova <michaela.farova@gmail.com>
2015-07-22 20:14:37 -0400

    Adding default settings option

--- DecisionAlgorithm/main.cpp
+++ DecisionAlgorithm/main.cpp
@@ -11,25 +11,8 @@ int main()
 	TestMain::runTests();
 
 	printf("Running Algorithm\n");
-	DecisionSettings settings;
-	settings.m_countWeight = 1.0;
 
-	settings.m_voteWeight = DecisionSettings::VoteWeight_Equal;
-
-	settings.m_songGenreWeight = DecisionSettings::VoteWeight_Equal;
-	settings.m_artistGenreWeight = DecisionSettings::VoteWeight_Equal;
-
-	settings.m_tierMultipliers[Tier_SAG] = 1.0f;
-	settings.m_tierMultipliers[Tier_SA] = 0.9f;
-	settings.m_tierMultipliers[Tier_SG] = 0.8f;
-	settings.m_tierMultipliers[Tier_AG] = 0.7f;
-	settings.m_tierMultipliers[Tier_S] = 0.6f;
-	settings.m_tierMultipliers[Tier_A] = 0.5f;
-	settings.m_tierMultipliers[Tier_G] = 0.4f;
-
-	settings.m_numSongsGenerated = 3;
-	
-	DecisionAlgorithm algorithm(settings);
+	DecisionAlgorithm algorithm(DecisionSettings::default());
 
 	while (true) {
 		algorithm.run();
--- DecisionAlgorithm/src/Common/DecisionSettings.h
+++ DecisionAlgorithm/src/Common/DecisionSettings.h
@@ -25,6 +25,28 @@ struct DecisionSettings {
 
 	int m_numSongsGenerated;
 
+	static DecisionSettings default() {
+		DecisionSettings settings;
+		settings.m_countWeight = 1.0;
+
+		settings.m_voteWeight = DecisionSettings::VoteWeight_Equal;
+
+		settings.m_songGenreWeight = DecisionSettings::VoteWeight_Equal;
+		settings.m_artistGenreWeight = DecisionSettings::VoteWeight_Equal;
+
+		settings.m_tierMultipliers[Tier_SAG] = 1.0f;
+		settings.m_tierMultipliers[Tier_SA] = 0.9f;
+		settings.m_tierMultipliers[Tier_SG] = 0.8f;
+		settings.m_tierMultipliers[Tier_AG] = 0.7f;
+		settings.m_tierMultipliers[Tier_S] = 0.6f;
+		settings.m_tierMultipliers[Tier_A] = 0.5f;
+		settings.m_tierMultipliers[Tier_G] = 0.4f;
+
+		settings.m_numSongsGenerated = 3;
+		
+		return settings;
+	}
+
 };
 
 #endif
